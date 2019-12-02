69dac0f094ec9c40e8f820aaa1e950be075e2ddd
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-02 17:02:29 +0500

    add input box icon

--- src/Screens/User-Profile.js
+++ src/Screens/User-Profile.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import {Container, Content, Form, Item, Input, Text, Button} from 'native-base';
-import {StyleSheet} from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Text,
+  Button,
+  Icon,
+} from 'native-base';
+import {StyleSheet, View} from 'react-native';
 
 const UserProfile = () => {
   return (
     <Container>
       <Content>
         <Form style={styles.formStyle}>
-          <Item>
-            <Input style={styles.inputBox} placeholder="Name" />
-          </Item>
-          <Item>
-            <Input style={styles.inputBox} placeholder="Email" />
-          </Item>
-          <Item>
-            <Input
-              keyboardType="numeric"
-              style={styles.inputBox}
-              placeholder="Contact"
-            />
-          </Item>
+          <View style={styles.inputBox}>
+            <Item>
+              <Icon name="user" type="AntDesign" />
+              <Input placeholder="Name" />
+            </Item>
+          </View>
+          <View style={styles.inputBox}>
+            <Item>
+              <Icon name="email" type="MaterialIcons" />
+              <Input placeholder="Email" />
+            </Item>
+          </View>
+          <View style={styles.inputBox}>
+            <Item>
+              <Icon name="phone" type="Feather" />
+              <Input keyboardType="numeric" placeholder="Contact" />
+            </Item>
+          </View>
           <Button style={styles.BtnStyle}>
             <Text style={styles.btnText}> UPDATE </Text>
           </Button>
@@ -42,7 +56,6 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#000',
     backgroundColor: '#fff',
-    padding: 10,
     paddingLeft: 10,
     fontFamily: 'MavenPro-Regular',
     fontSize: 13,
