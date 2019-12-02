a5107b2f00f38b1ad5c343c1140dbd20cd8c0eaa
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-02 11:06:49 +0500

    routing pakages installation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {StatusBar} from 'react-native';
 import AddTodo from './Screens/Add-Todo';
-import {Container, Header, Content, Form, Item, Input} from 'native-base';
+
 const App = () => {
   return (
     <>
